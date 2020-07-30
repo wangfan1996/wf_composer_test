5276863b6bf21b51b0413456283bbfcb35e97ac3
王帆 <1356316925@qq.com>
2020-07-30 17:13:55 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm
+ * User: 王帆
+ * Date: 2020-07-30
+ * Time: 17:09
+ */
+
+require "vendor/autoload.php";
+
+$Test = new Siam\Test();
+$Test->test();
---
+++ src/siam/Test.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm
+ * User: 王帆
+ * Date: 2020-07-30
+ * Time: 17:09
+ */
+
+namespace Siam;
+class Test
+{
+    function test()
+    {
+        echo "Form Test -> test()";
+    }
+}
